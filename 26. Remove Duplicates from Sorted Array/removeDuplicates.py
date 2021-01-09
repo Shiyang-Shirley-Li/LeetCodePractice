b59ce20b0c19609d6f12068657848cfdb858b950
Shiyang(Shirley) Li <76845654+Shiyang-Shirley-Li@users.noreply.github.com>
2021-01-09 09:29:02 -0700

    First attempt

---
+++ 26. Remove Duplicates from Sorted Array/removeDuplicates.py
@@ -0,0 +1,7 @@
+"""
+Tag: Array
+Difficulty: Easy
+Approach: Two Pointers
+Analysis:
+  1. The array is sorted, we can keep two pointers i and j, where i is the slow-runner while j is the fast-runner
+"""
