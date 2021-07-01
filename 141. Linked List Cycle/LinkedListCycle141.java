52a2dfdf86b7e90a888d7628a97638df965a95fa
Shiyang(Shirley) Li <76845654+Shiyang-Shirley-Li@users.noreply.github.com>
2021-06-30 19:17:59 -0600

    Need more time

---
+++ 141. Linked List Cycle/LinkedListCycle141.java
@@ -0,0 +1,50 @@
+
+/*
+ * Author: Shiyang (Shirley) Li
+ * Tag: Two Pointers
+ * Difficulty： Easy
+ * 
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached 
+ * again by continuously following the next pointer. Internally, pos is used to denote the 
+ * index of the node that tail's next pointer is connected to. Note that pos is not passed 
+ * as a parameter. 
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle141 {
+	
+	public boolean hasCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			if(slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
+
+/*
+ * Conclusion:
+ * 	1. Two pointers: slow and fast
+ *  2. Like a chasing problem. If there is a cycle, the fast will chase the slow again.
+ */
